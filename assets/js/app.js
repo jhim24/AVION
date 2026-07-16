00d4edad3342b664014614b634873b359140a70e
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-16 20:02:46 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,33 @@
+// =========================
+// MOBILE MENU
+// =========================
 
+const menuBtn=document.querySelector(".menu-btn");
+
+const mobileMenu=document.querySelector(".mobile-menu");
+
+menuBtn.onclick=()=>{
+
+mobileMenu.classList.toggle("active");
+
+}
+
+// =========================
+// NAVBAR SCROLL
+// =========================
+
+const navbar=document.querySelector(".navbar");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>80){
+
+navbar.classList.add("scrolled");
+
+}else{
+
+navbar.classList.remove("scrolled");
+
+}
+
+});
